28cfedffe9560474fbde44f3719898dda28286ff
Tomoya Inada <inack179@gmail.com>
2023-02-17 00:00:24 +0900

    In the middle of learning.

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import { Login } from "../components/pages/Login";
 import { Page404 } from "../components/pages/Page404";
-import { HeaderLayout } from "../components/templatesA/HeaderLayout";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 import { homeRoutes } from "./HomeRoutes";
 
 export const Router: VFC = memo(() => {
