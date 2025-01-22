28e6854ea2b18a6bd4d5e10fa241c70b0d5f4e52
tomoya inada <60378839+TomoyaInada@users.noreply.github.com>
2025-01-22 07:17:38 +0000

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
 // コメントを追加するだけです。
+// もう一度コメント（テストです）
 import theme from "./theme/theme";
 import { Router } from "./router/Router";
 
